bfdb7b3dbe5ae0d733795cc26688bc93bc7b333a
wfnuser <wfnuser@wfnusers-MacBook-Air.local>
2015-04-30 13:14:28 +0800

    add the day_gone and the day_coming

--- js/calendar.js
+++ js/calendar.js
@@ -19,6 +19,7 @@ Calendar = function(){
 			var that = this;
 			$.extend(true, config, customConfig);
 			$('#' + config.cal_id).append(html_content);
+			$('#' + config.cal_id).addClass("linear");
 			$('#' + config.cal_id + ' .cal_top').html(text_top);
 			$('#' + config.cal_id + ' .cal_bt_month_left').bind('click',function(){
 				if(--cur_month<0){
@@ -46,17 +47,39 @@ Calendar = function(){
 		//warning to add parameter relative:getDateList
 		getDateList: function() {
 			var strCont ='';
-			for (j=-new Date(cur_year,cur_month,1).getDay();j;j++) {
-				strCont += '<span class="cal_date_gone">&nbsp;</span>';
+			var pre_month = cur_month - 1;
+			var pre_year = cur_year; //here the year means the year of next month or pre month
+			if(pre_month<0){
+				pre_month = pre_month + 12;
+				pre_year--;
 			}
+			var nxt_month = cur_month + 1;
+			var nxt_year = cur_year;
+			if(nxt_month>11){
+				nxt_month = pre_month - 12;
+				nxt_year++;
+			}
+			var pre_monthdays = new Date(pre_year, pre_month+1, 0).getDate();
+			var nxt_monthdays = new Date(nxt_year, nxt_month+1, 0).getDate();
 			var monthdays = new Date(cur_year,cur_month+1,0).getDate();
+			var lastday = new Date(cur_year,cur_month,monthdays).getDay();
+			
+			for (j=-new Date(cur_year,cur_month,1).getDay();j;j++) {
+				strCont += '<span class="cal_date_gone">' + (pre_monthdays+j+1) + '</span>';
+			}
 			while(++j<= monthdays){
 				strCont += '<span>' + j + '</span>';
 			}
+			if(lastday!=6)
+			{
+				for(j=1;j<=(6-lastday);j++)
+					strCont += '<span class="cal_date_coming">' + j + '</span>';
+			}
+			
 			$('#' + config.cal_id + ' .cal_date_content').html(strCont);
 			var dateelements = $('#' + config.cal_id + ' .cal_date_content span').not('.cal_date_gone');
 			//title line
-			$('#' + config.cal_id + ' .cal_month').html(cur_month+1 + "月 " + cur_year);
+			$('#' + config.cal_id + ' .cal_month').html(cur_year + "年" + (cur_month+1) + "月 " );
 			//sign today
 			if (now.getFullYear() == cur_year && now.getMonth() == cur_month) {
 				dateelements.filter(':contains(' + now.getDate() + ')').eq(0).attr("class", "cal_date_now");
